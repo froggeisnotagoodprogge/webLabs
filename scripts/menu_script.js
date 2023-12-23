b885ccecf04e3cc88371c0adf7cd65b0af6b7a55
froggeisnotagoodprogge <91646009+froggeisnotagoodprogge@users.noreply.github.com>
2023-12-23 14:57:04 +0300

    lab2-3

---
+++ scripts/menu_script.js
@@ -0,0 +1,28 @@
+(function () {
+function highlightActiveMenuItem() {
+    const menuItems = document.querySelectorAll('.menu-item');
+    menuItems.forEach(item => {
+        if (window.location.pathname.includes(item.getAttribute('href'))) {
+            item.classList.add('active');
+            console.log('Active class added to:', item);
+        }
+    });
+}
+function addMenuEventListeners() {
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menuItems.forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            item.classList.add('hovered');
+        });
+
+        item.addEventListener('mouseleave', () => {
+            item.classList.remove('hovered');
+        });
+    });
+}
+
+window.addEventListener('load', () => {
+    highlightActiveMenuItem();
+    addMenuEventListeners();});
+})();
